e95da8ebf122ca812b13ea9989d4ef0697e98903
Thor <thorlindberg@icloud.com>
2021-04-06 22:32:47 +0200

    Initial Commit

---
+++ Shared/LocalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalsApp.swift
+//  Shared
+//
+//  Created by Thor Lindberg on 06/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct LocalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
