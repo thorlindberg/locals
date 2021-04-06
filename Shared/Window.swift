a5c3afde2e8096d44771e8d0fb113edd39fe1995
Thor <thorlindberg@icloud.com>
2021-04-07 00:02:11 +0200

    Restructured interface and navigation. Ready for implementation!

--- Shared/Window.swift
+++ Shared/Window.swift
@@ -1,6 +1,7 @@
 import SwiftUI
 
 struct Window: View {
+    @State var inspector: Bool = true
     var body: some View {
         NavigationView {
             List {
@@ -10,75 +11,81 @@ struct Window: View {
             }
             .frame(minWidth: 230)
             .toolbar {
-                ToolbarItemGroup(placement: .automatic) {
+                ToolbarItem(placement: .automatic) {
                     Button(action: {
                         NSApp.keyWindow?.firstResponder?.tryToPerform(#selector(NSSplitViewController.toggleSidebar(_:)), with: nil)
                     }) {
                         Image(systemName: "rectangle.leftthird.inset.fill")
                     }
-                    Button(action: {
-                        //
-                    }) {
-                        Text("Edit")
-                    }
                 }
             }
-            List {
-                VStack(spacing: 15) {
-                    ForEach(0 ..< 100) { _ in
-                        ZStack {
-                            Rectangle()
-                                .opacity(0.05)
-                                .cornerRadius(10)
-                                .frame(minHeight: 50)
-                            HStack(spacing: 15) {
-                                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
-                                    .foregroundColor(.accentColor)
-                                Divider()
-                                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
-                                Spacer()
-                                VStack {
-                                    Image(systemName: "pin.fill")
-                                        .opacity(0.3)
-                                    Spacer()
+            VStack(spacing: 0) {
+                Divider()
+                HStack(spacing: 0) {
+                    List {
+                        VStack(spacing: 15) {
+                            ForEach(0 ..< 100) { _ in
+                                ZStack {
+                                    Rectangle()
+                                        .opacity(0.05)
+                                        .cornerRadius(10)
+                                        .frame(minHeight: 50)
+                                    HStack(spacing: 15) {
+                                        Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
+                                            .foregroundColor(.accentColor)
+                                        Divider()
+                                        Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
+                                        Spacer()
+                                        VStack {
+                                            Image(systemName: "pin") // onTapGesture: CHANGE TO FILL AND HIGHER OPACITY
+                                                .opacity(0.3)
+                                            Spacer()
+                                        }
+                                    }
+                                    .padding()
                                 }
+                            }
+                        }
+                    }
+                    if inspector {
+                        Divider()
+                        ScrollView {
+                            VStack(spacing: 15) {
+                                HStack {
+                                    Text("Translate")
+                                    Spacer()
+                                    Button(action: {
+                                        //
+                                    }) {
+                                        Text("Translate")
+                                    }
+                                }
+                                Divider()
                             }
                             .padding()
                         }
+                        .frame(width: 230)
                     }
                 }
             }
             .navigationTitle("")
             .toolbar {
                 ToolbarItem(placement: .navigation) {
-                    Menu("Menu") {
+                    Menu {
+                        //
+                    } label: {
+                        Text("Welcome to Xlocal")
+                            .font(.system(size: 10))
                     }
-                    .frame(width: 200)
+                    .background(Color("StatusColor"))
+                    .frame(width: 320)
+                    .cornerRadius(5)
+                    .help("View project changelog, and revert to a previous version")
                 }
-                ToolbarItem(placement: .automatic) {
+                ToolbarItemGroup(placement: .automatic) {
                     TextField("􀊫 Search", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                         .frame(minWidth: 205)
-                }
-            }
-            List {
-                VStack(spacing: 15) {
-                    HStack {
-                        Text("Inspector")
-                        Spacer()
-                        Button(action: {
-                            //
-                        }) {
-                            Text("Inspector")
-                        }
-                    }
-                    Divider()
-                }
-            }
-            .frame(minWidth: 230)
-            .toolbar {
-                ToolbarItemGroup(placement: .automatic) {
-                    Spacer()
                     Button(action: {
                         //
                     }) {
@@ -88,6 +95,9 @@ struct Window: View {
                         //
                     }) {
                         Image(systemName: "square.and.arrow.up")
+                    }
+                    Toggle(isOn: $inspector) {
+                        Image(systemName: "rectangle.rightthird.inset.fill")
                     }
                 }
             }
